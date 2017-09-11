b44b263aac9b5026e9414da844e6c03f2a8175c9
Arsalan Savand <arsalan@savandbros.com>
2017-09-11 23:13:44 +0400

    Add id to Account model

--- src/models/account.ts
+++ src/models/account.ts
@@ -5,12 +5,14 @@ import {Media} from "./media";
 class User {
   name: string;
   username: string;
+  id: string;
   media: Media;
   sites: Array<UserSite> = [];
 
-  constructor(name: string, username:string, media: Media, sites: Array<UserSite>) {
+  constructor(name: string, username:string, id: string, media: Media, sites: Array<UserSite>) {
     this.name = name;
     this.username = username;
+    this.id = id;
     this.media = media;
     this.sites = sites;
   }
@@ -67,6 +69,6 @@ export class Account {
     for (let siteData of data.sites) {
       sites.push(new UserSite(siteData['id'], siteData['role'], siteData['title'], siteData['url']));
     }
-    this.user = new User(data.name, data.username, media, sites);
+    this.user = new User(data.name, data.username, data.id, media, sites);
   }
 }
