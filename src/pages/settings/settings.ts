d337efd05b5456f36c428142113f8b0067f26074
Arsalan Savand <arsalan@savandbros.com>
2017-09-19 05:37:55 +0400

    Update SettingsPage

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, LoadingController, NavController, ToastController} from 'ionic-angular';
 import {Site} from "../../models/site";
 import {AuthServiceProvider} from "../../providers/auth-service/auth-service";
 import {SiteProvider} from "../../providers/site/site";
+import {DomSanitizer, SafeStyle} from "@angular/platform-browser";
 
 @IonicPage()
 @Component({
@@ -12,12 +13,51 @@ import {SiteProvider} from "../../providers/site/site";
 export class SettingsPage {
 
   site: Site = new Site({});
+  coverImage: SafeStyle;
+  updating: boolean;
 
-  constructor(public navCtrl: NavController, public siteService: SiteProvider) {
+  constructor(public navCtrl: NavController, public siteService: SiteProvider, public sanitizer: DomSanitizer,
+              public loadingCtrl: LoadingController, public authService: AuthServiceProvider, public toastCtrl: ToastController) {
+
+    let loader = this.loadingCtrl.create({content: "Please wait..."});
+    loader.present();
+
     this.siteService.site().subscribe((resp) => {
+      loader.dismiss();
+
       this.site = resp;
+      if (this.site.media.coverImage) {
+        this.coverImage = this.sanitizer.bypassSecurityTrustStyle(`url(${this.site.media.coverImage.file})`);
+      }
     }, (err) => {
+      loader.dismiss();
       console.log(err);
     });
+  }
+
+  updateSite() {
+    this.updating = true;
+
+    let payload = {
+      title: this.site.title,
+      description: this.site.description,
+      meta_description: this.site.metaDescription,
+      commenting: this.site.commenting,
+      voting: this.site.voting
+    };
+
+    this.siteService.updateSite(payload).subscribe((resp) => {
+      this.updating = false;
+      this.site = resp;
+      let toast = this.toastCtrl.create({
+        message: 'Site updated',
+        duration: 3000,
+        position: 'bottom'
+      });
+      toast.present();
+    }, (err) => {
+      this.updating = false;
+      console.log(err);
+    })
   }
 }
