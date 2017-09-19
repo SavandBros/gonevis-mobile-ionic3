eeb8a06393fc5b0b88d1ea50425a2a81ac3a8c77
Arsalan Savand <arsalan@savandbros.com>
2017-09-19 05:39:23 +0400

    Add PUT method to SiteProvider and change GET method endpoint

--- src/providers/site/site.ts
+++ src/providers/site/site.ts
@@ -1,10 +1,11 @@
-import {Injectable} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
 import {Response} from '@angular/http';
 import 'rxjs/add/operator/map';
 import {Site} from "../../models/site";
 import {Api} from "../api";
 import {AuthServiceProvider} from "../auth-service/auth-service";
 import 'rxjs/add/operator/map';
+import {Account} from "../../models/account";
 
 /*
   Generated class for the SiteProvider provider.
@@ -15,14 +16,31 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class SiteProvider {
 
+  siteId: string;
+  currentSite: any;
+  public siteUpdated$: EventEmitter<Account> = new EventEmitter();
+
   constructor(public authService: AuthServiceProvider, public api: Api) {
+    this.siteId = this.authService.getCurrentSite().id;
+    this.currentSite = this.authService.getCurrentSite();
   }
 
   site() {
-    return this.api.get("website/site/" + this.authService.getCurrentSite().id + "/")
+    return this.api.get("website/site/" + this.siteId + "/settings/")
       .map((res: Response) => {
         let data = res.json();
         data = new Site(data);
+        return data;
+      });
+  }
+
+  updateSite(payload) {
+    return this.api.put("website/site/" + this.siteId + "/update-settings/", payload)
+      .map((res: Response) => {
+        let data = res.json();
+        data = new Site(data);
+
+        this.siteUpdated$.emit(data);
         return data;
       });
   }
