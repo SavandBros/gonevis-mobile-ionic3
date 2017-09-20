bf66248bcf9f5276b42c951b81bf6623f85efed5
Alireza Savand <alireza.savand@gmail.com>
2017-09-20 06:08:52 +0400

    Much cleaner EntryProvider

--- src/providers/entry/entry.ts
+++ src/providers/entry/entry.ts
@@ -1,10 +1,15 @@
 import 'rxjs/add/operator/map';
 import {Entry} from "../../models/entry";
 import {BaseModelProvider} from "../base-model/base-model";
+import {JwtInterceptorProvider} from "../jwt-interceptor/jwt-interceptor";
+import {AuthServiceProvider} from "../auth-service/auth-service";
 
 
 export class EntryProvider extends BaseModelProvider<Entry> {
-  public apiEndPoint: string = "website/entry/{{site}}/";
-  public apiEndPointList: string = "website/entry/";
-  public modelClass = Entry;
+  constructor(public http: JwtInterceptorProvider, public authService: AuthServiceProvider){
+    super(http, authService);
+    this.apiEndPoint = "website/entry/{{site}}/";
+    this.apiEndPointList = "website/entry/";
+    this.modelClass = Entry
+  }
 }
