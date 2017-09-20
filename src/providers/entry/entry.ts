d3817d50ba59b311b6324f3b145b4cdfc5c4cda8
Alireza Savand <alireza.savand@gmail.com>
2017-09-20 06:02:29 +0400

    Making EntryProvider use BaseModelProvider

--- src/providers/entry/entry.ts
+++ src/providers/entry/entry.ts
@@ -1,32 +1,10 @@
-import {Injectable} from '@angular/core';
-import {Response} from '@angular/http';
 import 'rxjs/add/operator/map';
-import {Api} from "../api";
-import {AuthServiceProvider} from "../auth-service/auth-service";
 import {Entry} from "../../models/entry";
+import {BaseModelProvider} from "../base-model/base-model";
 
-/*
- Generated class for the EntryProvider provider.
 
- See https://angular.io/docs/ts/latest/guide/dependency-injection.html
- for more info on providers and Angular DI.
- */
-@Injectable()
-export class EntryProvider {
-
-  constructor(public api: Api, public authService: AuthServiceProvider) {}
-
-  entries() {
-    return this.api.get("website/entry/", {site: this.authService.getCurrentSite().id})
-      .map((res: Response) => {
-        let data = res.json();
-        let entries: Array<Entry> = [];
-        for (let entryData of data.results) {
-          entries.push(new Entry(entryData));
-        }
-
-        data.results = entries;
-        return data;
-      });
-  }
+export class EntryProvider extends BaseModelProvider<Entry> {
+  public apiEndPoint: string = "website/entry/{{site}}/";
+  public apiEndPointList: string = "website/entry/";
+  public modelClass = Entry;
 }
