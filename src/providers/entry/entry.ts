6d78fa2a2c270132dad6715cdfc40f78a50a96d9
Arsalan Savand <arsalan@savandbros.com>
2017-10-17 21:06:54 +0400

    Add PUT and POST method

--- src/providers/entry/entry.ts
+++ src/providers/entry/entry.ts
@@ -24,4 +24,21 @@ export class EntryProvider extends BaseModelProvider<Entry> {
       })
   }
 
+  update(entry: Entry) {
+    return this.http.put(`http://draft.gonevis.com/api/v1/website/entry/${entry.id}/`, entry)
+      .map((res: Response) => {
+
+        console.log(res.json());
+        return res.json();
+      })
+  }
+
+  create(entry: Entry) {
+    return this.http.post("http://draft.gonevis.com/api/v1/website/entry/", entry)
+      .map((res: Response) => {
+
+        console.log(res.json());
+        return res.json();
+      })
+  }
 }
