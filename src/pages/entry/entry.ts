232d561d1859a7a22189ae16087d7dba6a7e1d38
Arsalan Savand <arsalan@savandbros.com>
2017-10-17 21:06:43 +0400

    Improve code

--- src/pages/entry/entry.ts
+++ src/pages/entry/entry.ts
@@ -1,7 +1,8 @@
 import {Component} from '@angular/core';
 import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {Entry} from "../../models/entry";
-
+import {EntryProvider} from "../../providers/entry/entry";
+import {AuthProvider} from "../../providers/auth/auth-service";
 
 @IonicPage()
 @Component({
@@ -10,8 +11,48 @@ import {Entry} from "../../models/entry";
 })
 export class EntryPage {
   entry: Entry;
+  editing: boolean;
+  submitText: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.entry = this.navParams.get("entry");
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              public entryService: EntryProvider, public authService: AuthProvider) {
+
+    if (this.navParams.get("entry")) {
+      this.entry = <Entry> JSON.parse(JSON.stringify(this.navParams.get("entry")));
+      this.editing = true;
+      this.submitText = "Update";
+    } else {
+      this.editing = false;
+      this.submitText = "Publish";
+      this.entry = new Entry({});
+      this.entry.site = this.authService.getCurrentSite().id;
+    }
+  }
+
+  save() {
+    if (this.editing) {
+      this.update();
+    } else {
+      this.create();
+    }
+  }
+
+  update() {
+    this.entryService.update(this.entry).subscribe((resp) => {
+      console.log(resp);
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+
+  create() {
+
+    this.entryService.create(this.entry).subscribe((resp) => {
+      this.navCtrl.pop();
+
+    }, (err) => {
+      console.log(err);
+    });
   }
 }
