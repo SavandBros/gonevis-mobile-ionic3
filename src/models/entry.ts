bce7cdec20a67c56e86bf776271a2b6bfa3229c8
Arsalan Savand <arsalan@savandbros.com>
2018-01-23 08:55:09 +0400

    Update Entry Class

--- src/models/entry.ts
+++ src/models/entry.ts
@@ -2,10 +2,10 @@ import {DolphinFile} from "./dolphin-file";
 import {Tag} from "./tag";
 
 class EntryMedia {
-  cover_image: DolphinFile;
+  coverImage: DolphinFile;
 
-  constructor(cover_image: DolphinFile) {
-    this.cover_image = cover_image;
+  constructor(coverImage: DolphinFile) {
+    this.coverImage = coverImage;
   }
 
 }
@@ -15,54 +15,56 @@ export class Entry {
   title: string;
   content: string;
   user: string;
-  absolute_uri: string;
+  absoluteURI: string;
   site: string;
-  vote_count: number;
   slug: string;
   lead: string;
   excerpt: string;
-  meta_description: string;
+  metaDescription: string;
   status: number;
   format: number;
-  comment_enabled: boolean;
+  commentEnabled: boolean;
   featured: boolean;
   password: string;
-  view_count: number;
   published: string;
-  start_publication: string;
-  comment_count: number;
-  active_comment_count: number;
-  hidden_comment_count: number;
-  pending_comment_count: number;
+  startPublication: string;
+  commentCount: number;
+  viewCount: number;
+  voteCount: number;
+  activeCommentCount: number;
+  hiddenCommentCount: number;
+  pendingCommentCount: number;
   created: Date;
 
   media: EntryMedia;
   tags: Array<Tag> = [];
+
+  constStatus: number;
 
   constructor(data: any) {
     this.id = data.id;
     this.title = data.title;
     this.content = data.content;
     this.user = data.user;
-    this.absolute_uri = data.absolute_uri;
     this.site = data.site;
-    this.vote_count = data.vote_count;
     this.slug = data.slug;
     this.lead = data.lead;
     this.excerpt = data.excerpt;
-    this.meta_description = data.meta_description;
     this.status = data.status;
     this.format = data.format;
-    this.comment_enabled = data.comment_enabled;
     this.featured = data.featured;
     this.password = data.password;
-    this.view_count = data.view_count;
     this.published = data.published;
-    this.start_publication = data.start_publication;
-    this.comment_count = data.comment_count;
-    this.active_comment_count = data.active_comment_count;
-    this.hidden_comment_count = data.hidden_comment_count;
-    this.pending_comment_count = data.pending_comment_count;
+    this.viewCount = data.view_count;
+    this.voteCount = data.vote_count;
+    this.absoluteURI = data.absolute_uri;
+    this.commentEnabled = data.comment_enabled;
+    this.startPublication = data.start_publication;
+    this.metaDescription = data.meta_description;
+    this.commentCount = data.comment_count;
+    this.activeCommentCount = data.active_comment_count;
+    this.hiddenCommentCount = data.hidden_comment_count;
+    this.pendingCommentCount = data.pending_comment_count;
     this.created = new Date(data.created);
 
     let entryMedia = null;
@@ -76,5 +78,15 @@ export class Entry {
         this.tags.push(new Tag(tag));
       }
     }
+
+    this.constStatus = data.status;
+  }
+
+  getAbsoluteURI(): string {
+    if (this.constStatus == 0) {
+      return this.absoluteURI + "?view=preview";
+    }
+
+    return this.absoluteURI;
   }
 }
