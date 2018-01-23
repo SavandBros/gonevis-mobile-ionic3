13cd231ed57bd301289c58d5cc3d04b586c38da4
Arsalan Savand <arsalan@savandbros.com>
2018-01-23 08:53:05 +0400

    Add new provider "CodeKitProvider"

---
+++ src/providers/codekit/codekit.ts
@@ -0,0 +1,69 @@
+import {EventEmitter, Injectable} from '@angular/core';
+import {ActionSheetController, AlertController, ModalController, Platform} from "ionic-angular";
+import {DolphinSelectionPage} from "../../pages/dolphin-selection/dolphin-selection";
+import {DomSanitizer, SafeStyle} from '@angular/platform-browser';
+
+@Injectable()
+export class CodekitProvider {
+  public onImageRemoved$: EventEmitter<string> = new EventEmitter();
+
+  constructor(public actionSheetCtrl: ActionSheetController, public platform: Platform,
+              public alertCtrl: AlertController, public modalCtrl: ModalController, public sanitizer: DomSanitizer) {
+  }
+
+  bypassSecurityTrustStyle(coverImage: any): SafeStyle {
+    return this.sanitizer.bypassSecurityTrustStyle(`url(${coverImage})`);
+  }
+
+  options(image): void {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Options',
+      buttons: [
+        {
+          text: 'Change',
+          icon: !this.platform.is('ios') ? 'refresh' : null,
+          cssClass: 'action-icon-primary',
+          handler: () => this.selectImage(image)
+        },
+        {
+          text: 'Remove',
+          icon: !this.platform.is('ios') ? 'remove' : null,
+          role: 'Destructive',
+          cssClass: 'action-icon-danger',
+          handler: () => {
+            actionSheet.dismiss();
+
+            let alert = this.alertCtrl.create({
+              title: 'Are you sure?',
+              message: null,
+              buttons: [
+                {
+                  text: 'Cancel',
+                  role: 'cancel'
+                },
+                {
+                  text: 'Delete', handler: () => this.onImageRemoved$.emit(image)
+                }
+              ]
+            });
+            alert.present();
+
+            return false;
+          }
+        },
+        {
+          text: 'Cancel',
+          icon: !this.platform.is('ios') ? 'close' : null,
+          role: 'cancel'
+        },
+      ]
+    });
+
+    actionSheet.present();
+  }
+
+  selectImage(image: string): void | false {
+    let selectionModal = this.modalCtrl.create(DolphinSelectionPage, {source: image});
+    selectionModal.present();
+  }
+}
