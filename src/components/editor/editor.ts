b74d8d28346df8a7a7924378d311c222be0b6ccc
Arsalan Savand <arsalan@savandbros.com>
2018-01-30 16:56:02 +0400

    Improve code at Editor

--- src/components/editor/editor.ts
+++ src/components/editor/editor.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {EditorAction} from "./editor-action";
-import {Events} from "ionic-angular";
+import {AlertController, Events} from "ionic-angular";
+import {CodekitProvider} from "../../providers/codekit/codekit";
 
 
 @Component({
@@ -18,18 +19,24 @@ export class EditorComponent {
     'content': 'gonevis-editor-content',
   };
 
-  constructor(public events: Events) {
+  constructor(public events: Events, public codekit: CodekitProvider,
+              public alertCtrl: AlertController) {
     EditorComponent.event = this.events;
+
     setTimeout(() => {
       this.editable = document.getElementsByClassName("entry-content");
       EditorComponent.event.publish('entry:typing', this.editable.content.innerHTML);
 
       this.listener();
-    }, 0)
+    }, 0);
+
+    this.events.subscribe('image:selected', (dolphin) => {
+      if (dolphin) EditorComponent.exec('insertImage', dolphin.file);
+    });
   }
 
   listener() {
-    this.editable.content.addEventListener('input', function(container) {
+    this.editable.content.addEventListener('input', function (container) {
       EditorComponent.emitContent(container.target.innerHTML);
     });
   }
@@ -44,15 +51,78 @@ export class EditorComponent {
     new EditorAction("Heading 1", "<b>H<sub>1</sub></b>", () => EditorComponent.exec("formatBlock", "<H1>")),
     new EditorAction("Heading 2", "<b>H<sub>2</sub></b>", () => EditorComponent.exec("formatBlock", "<H2>")),
     new EditorAction("Unordered List", "&#8226", () => EditorComponent.exec("insertUnorderedList")),
-    new EditorAction("Link", "&#128279;", () => {
-      const url: string = window.prompt('Enter the link URL');
-      if (url) EditorComponent.exec('createLink', url)
-    }),
-    new EditorAction("Image", "&#128247;", () => {
-      const url: string = window.prompt('Enter the image URL');
-      if (url) EditorComponent.exec('insertImage', url)
-    }),
   ];
+
+  insertLink(): void {
+    let range: Range;
+    let sel: Selection;
+
+    if (window.getSelection().anchorNode && window.getSelection().getRangeAt(0)) {
+      sel = window.getSelection();
+      range = window.getSelection().getRangeAt(0);
+    }
+
+    let alert = this.alertCtrl.create({
+      title: 'Link',
+      enableBackdropDismiss: true,
+      inputs: [
+        {
+          name: 'text',
+          type: 'text',
+          placeholder: 'Text'
+        },
+        {
+          name: 'link',
+          type: 'url',
+          placeholder: 'https://example.com'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {}
+        },
+        {
+          text: 'Add',
+          handler: data => {
+            let link: string;
+
+            // Check if link starts with https://
+            if(data.link.indexOf("https://") !== 0) {
+              link = `https://${data.link}`;
+            } else {
+              link = data.link;
+            }
+
+            // Create element
+            let linkElement: Element = document.createElement('a');
+            linkElement.setAttribute('href', link);
+            linkElement.innerHTML = data.text ? data.text : link;
+
+            // Add created element to the current caret position
+            let node = range.createContextualFragment(linkElement.outerHTML);
+            range.insertNode(node);
+
+            let newNode = document.querySelectorAll('[href="' + link + '"]');
+
+            range.setStartAfter(newNode[0]);
+            range.setEndAfter(newNode[0]);
+            sel.removeAllRanges();
+            sel.addRange(range);
+
+            EditorComponent.emitContent(document.getElementsByClassName('entry-content')[0].innerHTML);
+          }
+        }
+      ]
+    });
+
+    alert.present();
+  }
+
+  public insertImage() {
+    this.codekit.selectImage("editorAddImage");
+  };
 
   static exec(command: string, value?: any): void {
     document.execCommand(command, false, value);
