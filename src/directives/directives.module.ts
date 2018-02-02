c630b566efea85c7657eb43ba82310996aabf82c
Arsalan Savand <arsalan@savandbros.com>
2018-02-02 16:01:51 +0400

    Add ParallaxDirective

---
+++ src/directives/directives.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { ParallaxDirective } from './parallax/parallax';
+@NgModule({
+	declarations: [ParallaxDirective],
+	imports: [],
+	exports: [ParallaxDirective]
+})
+export class DirectivesModule {}
